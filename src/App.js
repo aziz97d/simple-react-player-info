9c67b7eba559e3e5efa72295e2377e1902ad198f
Abdul Aziz <aziz12d97@gmail.com>
2021-03-01 23:40:32 +0600

    title update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
 import AllPlayer from './Components/AllPlayer/AllPlayer';
-
+import { Helmet } from 'react-helmet';
 
 function App() {
-  
+
   return (
     <div className="App">
+        <Helmet>
+          <title>UEFA - Football Team</title>
+        </Helmet>
         <AllPlayer></AllPlayer>
     </div>
     
